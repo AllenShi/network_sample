564a9d436de4d6d5e290c9d0740c346dfddf764a
Allen Shi <jinliangshi@gmail.com>
2017-10-29 22:08:07 +0800

    Add UDPServer

--- src/main/java/net/sjl/UDPServer.java
+++ src/main/java/net/sjl/UDPServer.java
@@ -6,14 +6,12 @@ import static java.util.Arrays.*;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
 
-public class UDPClient {
+public class UDPServer {
 
   public static void main(String[] args) {
     OptionParser parser = new OptionParser() {
       {
-        acceptsAll(asList("s", "server")).withRequiredArg().describedAs("the target UDP server name or ip to send").ofType(String.class);
         acceptsAll(asList("p", "port")).withRequiredArg().describedAs("the target UDP server port to send").ofType(Integer.class).defaultsTo(6789);
-        accepts("m").withRequiredArg().describedAs("the message to send").ofType(String.class);
       }
     };
 
@@ -21,18 +19,16 @@ public class UDPClient {
     try {
       parser.printHelpOn(System.out);
       OptionSet options = parser.parse(args);
-      aSocket = new DatagramSocket();
-      String msg  = options.has("m") ? (String)options.valueOf("m") : "";
-      byte[] m = new StringBuilder(msg).append(" from ").append(InetAddress.getLocalHost().getHostName()).toString().getBytes();
-      String host = options.has("s") ? (String)options.valueOf("s") : (String)options.valueOf("server");
-      InetAddress server = InetAddress.getByName(host);
       int serverPort = options.has("p") ? (Integer)options.valueOf("p") : (Integer)options.valueOf("port");
-      DatagramPacket request = new DatagramPacket(m, m.length, server, serverPort);
-      aSocket.send(request);
+      aSocket = new DatagramSocket(serverPort);
       byte[] buffer = new byte[1024];
-      DatagramPacket reply = new DatagramPacket(buffer, buffer.length);
-      aSocket.receive(reply);
-      System.out.println("Reply: " + new String(reply.getData()));
+      while(true) {
+        DatagramPacket request = new DatagramPacket(buffer, buffer.length);
+        aSocket.receive(request);
+        System.out.println("Received: " + new String(request.getData()));
+        DatagramPacket reply = new DatagramPacket(request.getData(), request.getLength(), request.getAddress(), request.getPort());
+        aSocket.send(reply);
+      }
     } catch(SocketException e) {
       System.out.println("Socket: " + e.getMessage());
     } catch(IOException e) {
